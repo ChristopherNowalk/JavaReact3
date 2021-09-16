7d5444dc928d0d895bd34ab321d053031445cb8b
stukruze <audiolaboratory@hotmail.com>
2021-09-16 12:57:41 -0400

    buttons done

---
+++ ng-simon/src/app/app.component.ts
@@ -0,0 +1,73 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ng-simon';
+
+  hid: boolean = true;
+  hid0: boolean = false;
+  hid1: boolean = true;
+  hid2: boolean = true;
+  hid3: boolean = true;
+  hid4: boolean = true;
+
+
+
+  onPress(cat: any){
+
+    if(cat == 'imc1'){
+      this.hid = false;
+      this.hid1 = true;
+      this.hid2 = false;
+      this.hid3 = false;
+      this.hid4 = false; 
+      window.setTimeout(()=>{
+        this.hid = true;
+        this.hid1 = false;
+     }, 500);
+    }
+    if(cat == 'imc2'){
+      this.hid = false;
+      this.hid1 = false;
+      this.hid2 = true; 
+      this.hid3 = false;
+      this.hid4 = false;
+      window.setTimeout(()=>{
+        this.hid = true;
+        this.hid2 = false;
+     }, 500);
+    }
+    if(cat == 'imc3'){
+      this.hid = false;
+      this.hid1 = false;
+      this.hid2 = false;
+      this.hid3 = true;
+      this.hid4 = false; 
+      window.setTimeout(()=>{
+        this.hid = true;
+        this.hid3 = false;
+     }, 500);
+    }
+    if(cat == 'imc4'){
+      this.hid = false;
+      this.hid1 = false;
+      this.hid2 = false;
+      this.hid3 = false;
+      this.hid4 = true; 
+      window.setTimeout(()=>{
+        this.hid = true;
+        this.hid4 = false;
+     }, 500);
+    }
+
+   
+  }
+}
+
+
+
+
